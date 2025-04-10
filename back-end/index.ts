7a00f2d5228fe008702d34cfeb6b3f65f28a337e
WikL12 <wuzy19970612@outlook.com>
2025-04-10 18:00:36 +0800

    ...

--- back-end/index.ts
+++ back-end/index.ts
@@ -5,16 +5,16 @@ const app = express();
 app.use(express.json());
 
 
-app.post('/api1', (req:Request, res:Response) => {
-    // 解决跨域
-    res.header('Access-Control-Allow-Origin', '*');
-    res.json({ message: 'Hello World!',success  : false });
-});
-app.post('/api2', (req:Request, res:Response) => {
-    // 解决跨域
-    res.header('Access-Control-Allow-Origin', '*');
-    res.json({ message: 'Hello World!',success  : true });
-});
+// app.post('/api1', (req:Request, res:Response) => {
+//     // 解决跨域
+//     res.header('Access-Control-Allow-Origin', '*');
+//     res.json({ message: 'Hello World!',success  : false });
+// });
+// app.post('/api2', (req:Request, res:Response) => {
+//     // 解决跨域
+//     res.header('Access-Control-Allow-Origin', '*');
+//     res.json({ message: 'Hello World!',success  : true });
+// });
 
 app.use('/user', useRouter)
 
---
+++ back-end/router/systemRoute.ts
@@ -0,0 +1,8 @@
+import express, { Request, Response } from "express";
+const router = express.Router();
+// 登录接口
+router.post('/login',)
+// 登出接口
+router.post('/logout',)
+// 注册接口
+router.post('/register',)
