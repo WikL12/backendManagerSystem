8ac25928aafb7bfb9ec487f96b7e12140c5f418c
WikL12 <wuzy19970612@outlook.com>
2025-07-17 15:33:02 +0800

    wzy preLoad

--- back-end/index.ts
+++ back-end/index.ts
@@ -6,6 +6,7 @@ import todoRoute from './router/todoRoute'
 import bodyParser from 'body-parser';
 import cookieParser from 'cookie-parser';
 import expressWs from 'express-ws'
+import path from 'node:path'
 const app = express();
 expressWs(app);
 // app.use(express.json({limit:'50mb'}));
@@ -13,14 +14,32 @@ expressWs(app);
 app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({extended:false}));
+// app.use(cors({
+//     origin: 'http://localhost:5173',
+//     credentials: true,
+// }))
+const allowedOrigins = ['http://localhost:5173', 'http://localhost:3100'];
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: function (origin:any, callback) {
+        if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+          callback(null, true);
+        } else {
+          callback(new Error('Not allowed by CORS'));
+        }
+      },
     credentials: true,
 }))
 app.use('/assets', express.static('assets'));
 app.use('/user', useRouter)
 app.use('/system', systemRouter)
 app.use('/todo', todoRoute)
+
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static(path.join(__dirname,'../font-end/react-app','dist')));
+    app.get('*',(req,res)=>{
+        res.sendFile(path.join(__dirname,'../font-end/react-app','dist','index.html'));
+    });
+}
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
 })
